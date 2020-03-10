16c5cfc13309883ffeebb9d2920cd075bb260d58
Colm Britton <colmjude@gmail.com>
2020-03-10 12:29:45 +0000

    Set up flask app with digital land assets

---
+++ application/blueprints/frontend/views.py
@@ -0,0 +1,13 @@
+import datetime
+
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    json)
+
+frontend = Blueprint('frontend', __name__, template_folder='templates')
+
+@frontend.route('/')
+def index():
+    return render_template('index.html')
