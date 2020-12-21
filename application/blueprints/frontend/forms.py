0fa46fc0ec020a3fe038f6b8d8aab905c0b9aba0
Colm Britton <colmjude@gmail.com>
2020-12-21 11:01:19 +0000

    Content tweaks

--- application/blueprints/frontend/forms.py
+++ application/blueprints/frontend/forms.py
@@ -5,5 +5,12 @@ from flask_wtf.file import FileField, FileRequired, FileAllowed
 class UploadForm(FlaskForm):
 
     upload = FileField(
-        "file", validators=[FileRequired(), FileAllowed(["csv", "xlsx", "xlsm"])]
+        "file",
+        validators=[
+            FileRequired(),
+            FileAllowed(
+                ["csv", "xlsx", "xlsm"],
+                message="You should upload a file with the extension .csv, .xlsx or .xlsm",
+            ),
+        ],
     )
